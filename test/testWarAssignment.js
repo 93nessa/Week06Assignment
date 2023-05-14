15d648e9977a59e80b3c5d85285257d1b632c856
93nessa <93nessa@gmail.com>
2023-05-14 10:07:07 -0600

    updated test

--- test/testWarAssignment.js
+++ test/testWarAssignment.js
@@ -1,6 +1,7 @@
+const chai =require ('chai')
 const expect = chai.expect
 const assert = chai.assert
-const{Deck, Player, Card} = require('./WarAssignment/WarAssignment.js');
+const{Deck, Player, Card} = require('../WarAssignment.js');
 
 
 
@@ -10,7 +11,17 @@ describe(`Creating  a new player`, function(){
    describe('Player', function(){
     it('Add a new player ', function(){
         let player1 = new Player('Vanessa')
-        expect(player1).to.equal('Vanessa')
+        expect(player1.name).be.a.string('Vanessa')
     }); 
    }); 
 });
+
+describe(`Adding a new card`, function(){
+    describe(`Card`, function(){
+        it(`Add a new card with rank, suit, and value`, function(){
+            let card1 = new Card(2, 2, '♣')
+            expect(card1.rank).to.equal(2)
+        });
+    });
+});
+  
